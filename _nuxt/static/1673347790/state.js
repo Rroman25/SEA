bb73944ca449420c31060617655fa7ed6a024454
Gleb Gurskiy <gleb@msa-it.ru>
2023-01-10 13:52:15 +0300

    Start 3

---
+++ _nuxt/static/1673347790/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSEA\u002F_nuxt\u002Fstatic\u002F1673347790",layout:"default",error:a,state:{content:{content:1,navId:a}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FSEA\u002F",assetsPath:"\u002FSEA\u002F_nuxt\u002F",cdnURL:a}}}}(null));
