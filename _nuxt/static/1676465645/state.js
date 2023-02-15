3f9fa7ff3a36fcf8587b56b511d2feafbce142ff
Rroman25 <122351624+rroman25@users.noreply.github.com>
2023-02-15 16:47:35 +0300

    fix build

--- _nuxt/static/1676465645/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSEA\u002F_nuxt\u002Fstatic\u002F1676465645",layout:"default",error:a,state:{content:{content:1,navId:a}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FSEA\u002F",assetsPath:"\u002FSEA\u002F_nuxt\u002F",cdnURL:a}}}}(null));
