3d62accfb1fc1ddae2b989fa22e94621f308261c
Rroman25 <122351624+rroman25@users.noreply.github.com>
2023-02-15 16:00:15 +0300

    fix opt

--- _nuxt/static/1673347165/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673347165",layout:"default",error:a,state:{content:{content:1,navId:a}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
---
+++ _nuxt/static/1676465645/thankyou/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSEA\u002F_nuxt\u002Fstatic\u002F1676465645",layout:"default",error:a,state:{content:{content:1,navId:a}},serverRendered:true,routePath:"\u002Fthankyou",config:{_app:{basePath:"\u002FSEA\u002F",assetsPath:"\u002FSEA\u002F_nuxt\u002F",cdnURL:a}}}}(null));
